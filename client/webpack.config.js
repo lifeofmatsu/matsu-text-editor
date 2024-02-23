8924150fa83290e15f543527be0c988a25aff712
Jus Ferrell <j1ferrel@ucsd.edu>
2024-02-22 21:39:02 -0800

    webpack config - wkbx plugins, css, babel

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,27 +4,75 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
+const plugins = [
+    new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'J.A.T.E',
+    }),
+    new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'service-worker.js',
+    }),
+    new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'JATE Text Editor',
+        short_name: 'JATE',
+        description: 'Just Another Text Editor!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+            {
+                src: path.resolve('src/assets/icon.png'),
+                sizes: [96, 128, 192, 256, 384, 512],
+                destination: path.join('assets', 'icons'),
+            },
+        ],
+    }),
+];
+
 // TODO: Add CSS loaders and babel to webpack.
+const module = {
+    rules: [
+        {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+        },
+        {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env'],
+                    plugins: ['@babel/plugin-transform-runtime'],
+                },
+            },
+        },
+    ],
+};
 
 module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      
-    ],
+    return {
+        mode: 'development',
+        entry: {
+            main: './src/js/index.js',
+            install: './src/js/install.js'
+        },
+        output: {
+            filename: '[name].bundle.js',
+            path: path.resolve(__dirname, 'dist'),
+        },
+        plugins: [
 
-    module: {
-      rules: [
-        
-      ],
-    },
-  };
+        ],
+
+        module: {
+            rules: [
+
+            ],
+        },
+    };
 };
