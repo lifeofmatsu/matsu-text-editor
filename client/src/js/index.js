3917158372ec8ee4920471e98b2fe18e06e3a09c
Jus Ferrell <j1ferrel@ucsd.edu>
2024-02-23 00:57:26 -0800

    ongoing debugging of undefined editor / infinite loadSpinner

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,36 +1,33 @@
-import { Workbox } from "workbox-window";
-import Editor from "./editor";
-import "./database";
-import "../css/style.css";
-console.log('1');
-const main = document.querySelector("#main");
-main.innerHTML = "";
+import { Workbox } from 'workbox-window';
+import Editor from './editor';
+import './database';
+import '../css/style.css';
+
+const main = document.querySelector('#main');
+main.innerHTML = '';
 
 const loadSpinner = () => {
-    console.log('2');
-	const spinner = document.createElement("div");
-	spinner.classList.add("spinner");
+	const spinner = document.createElement('div');
+	spinner.classList.add('spinner');
 	spinner.innerHTML = `
-  <div class="loading-container">
-  <div class="loading-spinner" />
-  </div>
-  `;
+	<div class="loading-container">
+	<div class="loading-spinner" />
+	</div>
+	`;
 	main.appendChild(spinner);
-    console.log('3');
 };
 
 const editor = new Editor();
-console.log('4');
-if (typeof editor === "undefined") {
-    console.log('5');
+
+if (typeof editor === 'undefined') {
 	loadSpinner();
 }
-console.log('6');
+
 // Check if service workers are supported
-if ("serviceWorker" in navigator) {
+if ('serviceWorker' in navigator) {
 	// register workbox service worker
-	const workboxSW = new Workbox("/src-sw.js");
+	const workboxSW = new Workbox('/src-sw.js');
 	workboxSW.register();
 } else {
-	console.error("Service workers are not supported in this browser.");
+	console.error('Service workers are not supported in this browser.');
 }
