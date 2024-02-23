f784ffec242ad17a8a86505eb6d54f51bba4a369
Jus Ferrell <j1ferrel@ucsd.edu>
2024-02-23 02:58:19 -0800

    implement PWA; resolved all route/path issues; all requirements have been met

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -37,7 +37,7 @@ module.exports = () => {
 				publicPath: '/',
 				icons: [
 					{
-						src: path.resolve('src/images/logo.png'),
+						src: path.resolve('./src/images/logo.png'),
 						sizes: [96, 128, 192, 256, 384, 512],
 						destination: 'icons',
 					},
